e023db63d0b8cbb11661dced68d8c79174427a11
yashar <yasharnajafi@gmailcom  install_system.sh>
2024-12-06 14:21:29 -0800

    Upadated the application start events

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from sqlalchemy import Column, Integer, Float, DateTime
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from alembic import command
+from alembic.config import Config
 from dotenv import load_dotenv
 
 
@@ -38,3 +40,14 @@ async def get_db():  # Dependency to get the async database session
 
 
 app = FastAPI()
+
+ALEMBIC_CONFIG = "alembic.ini"
+
+
+@app.on_event("startup")
+async def startup_event():
+    """Run migrations on application startup."""
+    alembic_cfg = Config(ALEMBIC_CONFIG)
+    command.upgrade(
+        alembic_cfg, "head"
+    )  # This will apply all migrations to the latest state upon running the app
